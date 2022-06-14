4fea5f7eaea12720209ae691c90d997883c01a13
sungsuhan <swhanssu@gmail.com>
2022-06-14 14:57:14 +0900

    2022-06-14 15 weather

--- src/main/java/kr/co/clozet/api/WeatherApiController.java
+++ src/main/java/kr/co/clozet/api/WeatherApiController.java
@@ -5,7 +5,6 @@ import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -13,7 +12,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
 
 /**
